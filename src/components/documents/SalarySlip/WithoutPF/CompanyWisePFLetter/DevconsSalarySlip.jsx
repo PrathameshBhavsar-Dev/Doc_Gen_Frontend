051f16de506deb7ca1c34c2db0f4d4e4a4163095
AnilJejurkar <aniljejurkar.9922@gmail.com>
2026-09-09 17:25:01 +0530

    Dev aliment

--- src/components/documents/SalarySlip/WithoutPF/CompanyWisePFLetter/DevconsSalarySlip.jsx
+++ src/components/documents/SalarySlip/WithoutPF/CompanyWisePFLetter/DevconsSalarySlip.jsx
@@ -114,7 +114,7 @@ const DevconsSalarySlip = ({ company = {}, data = {} }) => {
             fontFamily: "Bahnschrift",
             color: "#000",
             verticalAlign: "middle",         // 🔥 FIX alignment
-            lineHeight: 1.7,                 // 🔥 better spacing
+            lineHeight: 1.6,                 // 🔥 better spacing
           },
         }}
       >
